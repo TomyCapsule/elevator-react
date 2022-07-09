08616281f66f2d78ab553c93eb616742e1080976
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2022-07-09 15:40:48 +0200

    first deploy

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -27,6 +28,10 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
 // error handler
 app.use(function(err, req, res, next) {
